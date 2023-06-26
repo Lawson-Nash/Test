326623d471dfb6e43bfa1554b2ec690ca977b74c
Lawson Nash <nashlaw@sas.upenn.edu>
2023-06-26 10:17:01 -0400

    This is my first commitment of this week!

---
+++ newfile.java
@@ -0,0 +1,9 @@
+public class newFile {
+    public static void main(String[] args) {
+        int[] numbers = new int[10]; 
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(i); 
+        }
+    }
+}
